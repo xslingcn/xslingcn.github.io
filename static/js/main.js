3585a850cfa414f2155066ae07c5afec21c0752d
xslingcn <xslings@163.com>
2021-07-22 03:42:56 +0800

    add random pics

--- static/js/main.js
+++ static/js/main.js
@@ -50,3 +50,13 @@ $(document).ready(function() {
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
 });
+
+function setBackgroundImage(){
+  const jsdURL = "https://cdn.jsdelivr.net/npm/turna_assets@1.1.1/img/xsl.sh/"
+  var panelCover = document.getElementById("panel-cover");
+  var rndNum = Math.floor(Math.random() * 10).toString();
+  console.log(rndNum);
+  var destURL = jsdURL + rndNum + ".webp";
+  panelCover.setAttribute("style" ,"background-image: url('" + destURL + "')");
+
+}
